011eb3f3d3f588cfeeb3b7a37fce6c3c133bb10e
alansenairj <alansenairj@gmail.com>
2021-10-15 17:25:57 -0300

    slacko app e ex 3 jenkins

--- slacko/main.tf
+++ slacko/main.tf
@@ -1,18 +1,12 @@
-
-
-#variable "vpc_id" {
-#  type = string
-#}
-
-data "aws_ami" "slacko-app"  {
+data "aws_ami" "slacko-app"{
     most_recent = true
     owners = ["amazon"]
 
     filter {
         name = "name"
-        values = ["amazon*"]
+        values = ["Amazon*"]
     }
-    
+
     filter {
         name = "architecture"
         values = ["x86_64"]
@@ -20,180 +14,113 @@ data "aws_ami" "slacko-app"  {
 }
 
 data "aws_subnet" "subnet_public" {
-    cidr_block = "10.0.102.0/24"    
+    cidr_block = var.subnet_cidr
 }
 
-
-
-#variable "vpc_id" {}
-
-
-#https://www.youtube.com/watch?v=9cDDZzl7zow
-
-
-data "aws_vpc" "my-vpc" {
-    filter {
-      name ="tag:Name"
-      values = ["my-vpc"]
-
-    } 
-}
-
-#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
-
-#data "aws_vpc" "my-vpc" {
-#  id = "${var.vpc_id}"
-#}
-
-
-#variable "vpc_id" {} - não precisa de variável!
-
-#data "aws_vpc" "selected" {
-#  id = var.vpc_id
-#}
-
-#resource "aws_subnet" "example" {
-#  vpc_id            = data.aws_vpc.selected.id
-#
-#}
-
-
-
-
-# gerando a chave com o arquivo slacko.pub executando o comando no diretorio slacko-app
-# EX: vagrant@iaac-station:/vagrant/slacko-app$ ssh-keygen -C slacko -f slacko
-# sera gerado um arquivo slacko.pub, comandar um cat no arquivo e colar no campo public_key abaixo
-# EX: vagrant@iaac-station:/vagrant/slacko-app$ cat slacko.pub
-resource "aws_key_pair" "slacko-sshkey"{
-    key_name = "slacko-app-key"
-    #Colar todo conteudo do slacko.pub  entre as aspas 
-    public_key = file("slacko.pub")
-    
-
+# SSH Key
+resource "aws_key_pair" "slacko-sshkey" {
+    key_name = format("%s-app-key", var.app_name)
+    public_key = var.ssh_key
+  
 }
 
 resource "aws_instance" "slacko-app" {
     ami = data.aws_ami.slacko-app.id
-    instance_type = "t2.micro"
+    instance_type = var.app_instance
     subnet_id = data.aws_subnet.subnet_public.id
     associate_public_ip_address = true
-
-    tags = {
-        Name = "slacko-app"
-    }
-
+    tags = merge(var.app_tags,
+            {
+            "Name" = format("%s-app", var.app_name)
+            },)
     key_name = aws_key_pair.slacko-sshkey.id
-    # arquivo de bootstrap
-    user_data = file("ec2.sh")
+    # bootstrap file
+    user_data = file("${path.module}/files/ec2.sh")
+
 }
 
 resource "aws_instance" "mongodb" {
     ami = data.aws_ami.slacko-app.id
-    instance_type = "t2.small"
+    instance_type = var.db_instance
     subnet_id = data.aws_subnet.subnet_public.id
 
-    tags = {
-        Name = "mongodb"
-    }
-
+    tags = merge(var.app_tags,{
+      "Name" = format("%s-mongodb", var.app_name)
+    },)
     key_name = aws_key_pair.slacko-sshkey.id
-    # arquivo de bootstrap
-    user_data = file("mongodb.sh")
-}    
-
-resource "aws_security_group" "allow-slacko" {
-    name = "allow_ssh_http"
-    description = "allow ssh and http port"
-    # entrar na AWS e em VPC na segunda coluna tem a VPC ID, colar no campo abaixo https://console.aws.amazon.com/vpc/home?region=us-east-1#VpcDetails:VpcId=vpc-0291422ce6448f173
-   # vpc_id = "vpc-0291422ce6448f173"
-    
-    #teste vpc dinâmico
-    vpc_id = data.aws_vpc.my-vpc.id
-    
-    #"${data.aws_vpc.my-vpc.id}" 
-
-    ingress = [
-        {
-        description = "Allow SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = null
-
-    }
-    ,{
-        description = "Allow HTTP"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = null
-    }
-    ]
-
-    egress = [
-        {
-        description = "Allow all"
-        from_port = 0
-        to_port = 0
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = null
-        }
-    ]
-
-    tags = {
-        Name = "allow_ssh_http"
-    }
+    user_data = file("${path.module}/files/mongodb.sh")
 }
 
+resource "aws_security_group" "allow-slacko" {
+    name = format("%s-sg-app", var.app_name)
+    description = "allow ssh and http port"
+    vpc_id = var.vpc_id
+    tags = merge(var.app_tags,{
+        "Name" = format("%s-sg-app", var.app_name)
+    })
+}
+
+resource "aws_security_group_rule" "allow-slacko-ingress-http" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-slacko.id
+}
+
+resource "aws_security_group_rule" "allow-slacko-ingress-ssh" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-slacko.id
+}
+
+resource "aws_security_group_rule" "allow-slacko-egress-all" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-slacko.id
+}
+
+
+
 resource "aws_security_group" "allow-mongodb" {
-    name = "allow_mongodb"
-    description = "allow Mongodb"
-    # entrar na AWS e em VPC na segunda coluna tem a VPC ID, colar no campo abaixo
-    vpc_id = data.aws_vpc.my-vpc.id
+    name = format("%s-sg-db", var.app_name)
+    description = "allow nmongodb"
+    vpc_id = var.vpc_id
+    tags = merge(var.app_tags,{
+        "Name" = format("%s-sg-db", var.app_name)
+    })
+}
 
-    ingress = [
-        {
-        description = "allow mongodb"
-        from_port = 27017
-        to_port = 27017
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = null
-        }
-    ]
+resource "aws_security_group_rule" "allow-mongo-ingress-mongo" {
+    type = "ingress"
+    from_port = 27017
+    to_port = 27017
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
 
-    egress = [
-        {
-        description = "Allow all"
-        from_port = 0
-        to_port = 0
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = []
-        prefix_list_ids = []
-        security_groups = []
-        self = null
-        }
-    ]
-    
-    tags = {
-        Name = "allow_mongodb"
-    }
+    security_group_id = aws_security_group.allow-mongodb.id
+  
+}
+
+resource "aws_security_group_rule" "allow-mongo-egress-all" {
+    type = "egress"
+    from_port = 0
+    to_port = 0
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    security_group_id = aws_security_group.allow-mongodb.id
+  
 }
 
 resource "aws_network_interface_sg_attachment" "mongodb-sg" {
@@ -201,35 +128,23 @@ resource "aws_network_interface_sg_attachment" "mongodb-sg" {
     network_interface_id = aws_instance.mongodb.primary_network_interface_id
 }
 
-resource "aws_network_interface_sg_attachment" "slacko-sg" {
-    security_group_id = aws_security_group.allow-mongodb.id
+resource "aws_network_interface_sg_attachment" "slacko" {
+    security_group_id = aws_security_group.allow-slacko.id
     network_interface_id = aws_instance.slacko-app.primary_network_interface_id
 }
 
-resource "aws_route53_zone" "slack_zone"{
-    name = "iaac0506.com.br"
-
+resource "aws_route53_zone" "slacko_zone" {
+    name = format("%s-route53-zone-app", var.app_name)
     vpc {
-        # entrar na AWS e em VPC na segunda coluna tem a VPC ID, colar no campo abaixo
-        vpc_id = data.aws_vpc.my-vpc.id
+        vpc_id = var.vpc_id
     }
+    tags = var.app_tags
 }
 
-resource "aws_route53_record" "mongodb"{
-    zone_id = aws_route53_zone.slack_zone.id
-    name = "mongodb.iaac0506.com.br"
+resource "aws_route53_record" "mongodb" {
+    zone_id = aws_route53_zone.slacko_zone.id
+    name = format("%s-route53-rec-db", var.app_name)
     type = "A"
     ttl = "300"
     records = [aws_instance.mongodb.private_ip]
 }
-
-
-output "mongodb-host" {
-  value = aws_instance.mongodb.private_ip
-}
-
-output "slackapphost" {
-  value = aws_instance.slacko-app.public_ip
-}
-
-# para acessar http://(ip)/docs
